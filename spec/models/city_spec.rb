16137c045cd3ce8b36e485fcf2764ab86db2f2ea
Tyler Porter <tylerpporter@gmail.com>
2020-06-05 22:21:41 -0600

    Create model test for City

---
+++ spec/models/city_spec.rb
@@ -0,0 +1,10 @@
+require 'rails_helper'
+
+describe City do
+  describe 'validations' do
+    it { should validate_presence_of(:name) }
+    it { should validate_presence_of(:state) }
+    it { should validate_presence_of(:lat) }
+    it { should validate_presence_of(:lon) }
+  end
+end
