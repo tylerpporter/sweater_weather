14747a2e2826befeee66af285fbe587b980e0e1e
Tyler Porter <tylerpporter@gmail.com>
2020-06-06 11:10:42 -0600

    Create private helper methods

--- app/controllers/api/v1/forecasts_controller.rb
+++ app/controllers/api/v1/forecasts_controller.rb
@@ -1,14 +1,13 @@
 class Api::V1::ForecastsController < ApplicationController
   def show
-    City.create(city_params)
-    render json: City.last
-    # get the weather
-    # render JSON
+    city = find_or_create
+    forecast = Forecast.new(forecast_request(city))
+    render json: ForecastSerializer.new(forecast)
   end
 
   private
 
-  def city
+  def city_name
     params[:location].split(',').first
   end
 
@@ -16,13 +15,23 @@ class Api::V1::ForecastsController < ApplicationController
     params[:location].split(',').last
   end
 
+  def find_or_create
+    city = City.find_by(name: city_name)
+    city.nil? ? city = City.create(city_params) : city
+  end
+
+  def forecast_request(city)
+    OpenWeatherService.new(city.lat, city.lon, city.address).forecast
+  end
+
   def city_params
-    location = GeocodingService.new(city, state).lat_lon
+    location = GeocodingService.new(city_name, state).lat_lon
     {
-      name: city,
+      name: city_name,
       state: state,
-      lat: location[:lat],
-      lon: location[:lng]
+      lat: location[:location][:lat],
+      lon: location[:location][:lng],
+      address: location[:address]
     }
   end
 end
