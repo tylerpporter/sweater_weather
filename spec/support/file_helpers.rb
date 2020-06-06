2344cd2fb17185efaafdfc4b629b0e44cbddb7ca
Tyler Porter <tylerpporter@gmail.com>
2020-06-06 11:16:13 -0600

    Update module methods

--- spec/support/file_helpers.rb
+++ spec/support/file_helpers.rb
@@ -1,10 +1,5 @@
 module FileHelpers
-  def yaml_to_json(fixture_path)
-    YAML.load_file(fixture_path).to_json
-  end
-
-  def geo_response(fixture_path)
-    cassette = JSON.parse(yaml_to_json(fixture_path), symbolize_names: true)
-    JSON.parse(cassette[:http_interactions].first[:response][:body][:string], symbolize_names: true)
+  def parse_fixture(fixture_path)
+    JSON.parse(File.read(fixture_path), symbolize_names: true)
   end
 end
