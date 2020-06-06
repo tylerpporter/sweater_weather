f9d42f423d7e36898dce00695dbd7ba6538f23a6
Tyler Porter <tylerpporter@gmail.com>
2020-06-06 11:17:48 -0600

    Add test for returning forecast

--- spec/requests/api/v1/cities/lat_lon_spec.rb
+++ spec/requests/api/v1/cities/lat_lon_spec.rb
@@ -1,17 +1,33 @@
 require 'rails_helper'
 
 describe 'Geocoding Service' do
-  it 'retrieves the latitude and longitude of a given city', :vcr do
+  before :each do
+    @lat_lon = parse_fixture('spec/fixtures/geocode.json')
+    @lat = @lat_lon[:results].first[:geometry][:location][:lat].to_s
+    @lng = @lat_lon[:results].first[:geometry][:location][:lng].to_s
+    @forecast = parse_fixture('spec/fixtures/open_weather.json')
+    stub_geocoding_service
+    stub_open_weather_service(@lat,@lng)
+  end
+  it 'retrieves the latitude and longitude of a given city' do
     get "/api/v1/forecast?location=denver,co"
 
     city = City.last
-    cassette = geo_response('spec/fixtures/vcr_cassettes/Geocoding_Service/retrieves_the_latitude_and_longitude_of_a_given_city.yml')
-    lat = cassette[:results].first[:geometry][:location][:lat].to_s
-    lng = cassette[:results].first[:geometry][:location][:lng].to_s
 
     expect(city.name).to eq('denver')
     expect(city.state).to eq('co')
-    expect(city.lat).to eq(lat)
-    expect(city.lon).to eq(lng)
+    expect(city.lat).to eq(@lat)
+    expect(city.lon).to eq(@lng)
+  end
+  it 'retrieves the weather for a given city' do
+    get "/api/v1/forecast?location=denver,co"
+
+    city = City.last
+    resp = JSON.parse(response.body, symbolize_names: true)
+    
+    expect(resp[:data][:attributes][:address]).to eq(city.address)
+    expect(resp[:data][:attributes][:current]).to eq(@forecast[:current])
+    expect(resp[:data][:attributes][:hourly]).to eq(@forecast[:hourly])
+    expect(resp[:data][:attributes][:daily]).to eq(@forecast[:daily])
   end
 end
