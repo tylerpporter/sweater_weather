f4dc66dd8cf8389343e80ca9d7204f251e568093
Tyler Porter <tylerpporter@gmail.com>
2020-06-05 22:20:25 -0600

    Congifure shoulda-matchers

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -19,3 +19,9 @@ RSpec.configure do |config|
   config.filter_rails_from_backtrace!
   config.include FactoryBot::Syntax::Methods
 end
+Shoulda::Matchers.configure do |config|
+  config.integrate do |with|
+    with.test_framework :rspec
+    with.library :rails
+  end
+end
