6837cd1ccf47c86117bf5faaea9b677c16772e96
Tyler Porter <tylerpporter@gmail.com>
2020-06-06 11:17:11 -0600

    Refactor

--- app/services/open_weather_service.rb
+++ app/services/open_weather_service.rb
@@ -1,3 +1,30 @@
 class OpenWeatherService
+  def initialize(lat, lon, address)
+    @lat = lat
+    @lon = lon
+    @address = address
+  end
 
-end 
+  def forecast
+    {
+      address: @address,
+      resp: get_json(conn)
+    }
+  end
+
+  private
+
+  def conn
+    Faraday.get('https://api.openweathermap.org/data/2.5/onecall') do |req|
+      req.params["lat"] = @lat
+      req.params["lon"] = @lon
+      req.params["exclude"] = "minutely"
+      req.params["units"] = "imperial"
+      req.params["appid"] = ENV['WEATHER_KEY']
+    end
+  end
+
+  def get_json(resp)
+    JSON.parse(resp.body, symbolize_names: true)
+  end
+end
