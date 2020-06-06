dca9e82b53c6750795f107b021fa646c8fd3fdbe
Tyler Porter <tylerpporter@gmail.com>
2020-06-06 08:01:01 -0600

    Create GeocodingService

--- app/services/geocoding_service.rb
+++ app/services/geocoding_service.rb
@@ -1,3 +1,23 @@
 class GeocodingService
+  def initialize(city, state)
+    @city = city
+    @state = state
+  end
 
-end 
+  def lat_lon
+    get_json(conn)[:results].first[:geometry][:location]
+  end
+
+  private
+
+  def conn
+    Faraday.get('https://maps.googleapis.com/maps/api/geocode/json') do |req|
+      req.params["address"] = "#{@city}+#{@state}"
+      req.params["key"] = ENV['GEOCODING_KEY']
+    end
+  end
+
+  def get_json(resp)
+    JSON.parse(resp.body, symbolize_names: true)
+  end
+end
