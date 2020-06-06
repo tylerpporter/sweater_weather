cf0cfbff485fd2839ff4d94aa9c8f80b696ede9b
Tyler Porter <tylerpporter@gmail.com>
2020-06-06 11:17:25 -0600

    Update test to refelect new validations

--- spec/models/city_spec.rb
+++ spec/models/city_spec.rb
@@ -6,5 +6,6 @@ describe City do
     it { should validate_presence_of(:state) }
     it { should validate_presence_of(:lat) }
     it { should validate_presence_of(:lon) }
+    it { should validate_presence_of(:address) }
   end
 end
