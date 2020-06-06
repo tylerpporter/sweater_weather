78fd5db361bc05fbdd720b2f390279b2f1629029
Tyler Porter <tylerpporter@gmail.com>
2020-06-06 08:03:29 -0600

    Update RSpec helper files

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,5 +1,5 @@
-
 require 'spec_helper'
+
 ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../config/environment', __dir__)
 
@@ -18,10 +18,17 @@ RSpec.configure do |config|
   config.infer_spec_type_from_file_location!
   config.filter_rails_from_backtrace!
   config.include FactoryBot::Syntax::Methods
+
 end
 Shoulda::Matchers.configure do |config|
   config.integrate do |with|
     with.test_framework :rspec
     with.library :rails
   end
+end
+VCR.configure do |config|
+  config.cassette_library_dir = "spec/fixtures/vcr_cassettes"
+  config.hook_into :webmock
+  config.filter_sensitive_data('GEOCODING_KEY') { ENV['GEOCODING_KEY'] }
+  config.configure_rspec_metadata!
 end
