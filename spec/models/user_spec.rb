2cfe600ec7db26f947a1071ed0edb5b78234ad75
Tyler Porter <tylerpporter@gmail.com>
2020-06-08 15:50:41 -0600

    Write tests for User model

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,24 @@
 require 'rails_helper'
 
-RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe User do
+  describe 'validations' do
+    it { should validate_presence_of :email }
+    it { should validate_presence_of :password }
+    it { should validate_presence_of :password_confirmation }
+  end
+
+  describe 'instance methods' do
+    it 'can generate a unique api_key' do
+      user1 = create(:user)
+      user2 = create(:user)
+
+      expect(user1.api_key.length).to eq(22)
+      expect(user2.api_key.length).to eq(22)
+      expect(user1.api_key).to_not eq(user2.api_key)
+    end
+    it 'can return a status' do
+      user = create(:user)
+      expect(user.status).to eq(201)
+    end
+  end
 end
