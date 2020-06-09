147578414528909f0e92dd2f966c47c5a64ecd1f
Tyler Porter <tylerpporter@gmail.com>
2020-06-09 11:38:47 -0600

    Create login route

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
       get '/forecast', to: 'forecasts#show'
       get '/backgrounds', to: 'backgrounds#show'
       post '/users', to: 'users#create'
+      post '/sessions', to: 'sessions#create'
     end
   end
 end
