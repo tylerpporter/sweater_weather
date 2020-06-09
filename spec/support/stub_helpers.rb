9b550509cd109d2c0a94ad2d8cd372da3d51db92
Tyler Porter <tylerpporter@gmail.com>
2020-06-09 15:26:47 -0600

    Create stub helpers for road trip spec

--- spec/support/stub_helpers.rb
+++ spec/support/stub_helpers.rb
@@ -20,4 +20,21 @@ module StubHelpers
     stub_request(:get, 'https://api.teleport.org/api/urban_areas/slug:denver/images')
     .to_return(body: File.read('spec/fixtures/denver_image.json'))
   end
+
+  def stub_trip_service
+    stub_request(:get, "https://maps.googleapis.com/maps/api/directions/json?destination=Pueblo,CO&key=#{ENV['DIRECTIONS_KEY']}&origin=Denver,CO")
+    .with(headers: {
+       'Accept'=>'*/*',
+       'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3',
+       'User-Agent'=>'Faraday v1.0.1'
+      }
+    )
+    .to_return(status: 200, body: File.read('spec/fixtures/road_trip.json'), headers: {})
+  end
+
+  def stub_trip_weather
+    stub_request(:get, 'https://api.openweathermap.org/data/2.5/onecall')
+    .with(query: {'lat'=> '38.2542053', 'lon'=> '-104.6087488', 'exclude'=>'minutely', 'units'=>'imperial', 'appid'=> ENV['WEATHER_KEY']})
+    .to_return(body: File.read('spec/fixtures/road_trip_weather.json'))
+  end
 end
