49cdcbd48e61706616747346437c3ca41ecb2341
Tyler Porter <tylerpporter@gmail.com>
2020-06-09 11:37:27 -0600

    Create Login spec

---
+++ spec/requests/api/v1/login_spec.rb
@@ -0,0 +1,34 @@
+require 'rails_helper'
+
+describe 'Login response' do
+  before :each do
+    new_user = {
+      "email": "whatever@example.com",
+      "password": "password",
+      "password_confirmation": "password"
+    }
+    @user = User.create(new_user)
+  end
+  it 'should return status 200 if successful' do
+    credentials = {
+      "email": "whatever@example.com",
+      "password": "password"
+    }
+    post '/api/v1/sessions', params: credentials
+
+    expect(status).to eq(200)
+    expect(id).to eq(@user.id)
+    expect(attributes[:api_key]).to eq(@user.api_key)
+    expect(attributes[:email]).to eq(@user.email)
+  end
+  it 'should return status 400 if unsuccessful - bad credentials' do
+    credentials = {
+      "email": "whatever@example.com",
+      "password": "password22"
+    }
+    post '/api/v1/sessions', params: credentials
+
+    expect(status).to eq(400)
+    expect(message).to eq('Bad credentials')
+  end
+end
