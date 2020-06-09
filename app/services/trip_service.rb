1a32883c496538c8d01bf98226e6c3da93a500d8
Tyler Porter <tylerpporter@gmail.com>
2020-06-09 15:27:37 -0600

    Create Trip service

---
+++ app/services/trip_service.rb
@@ -0,0 +1,28 @@
+class TripService
+  include Servicable
+  def initialize(origin, destination)
+    @origin = origin
+    @destination = destination
+  end
+
+  def directions
+    raw = get_json(conn)[:routes].first[:legs].first
+    {
+      end_location: raw[:end_address],
+      travel_time: raw[:duration][:text],
+      unix_travel_time: raw[:duration][:value],
+      lat: raw[:end_location][:lat],
+      lng: raw[:end_location][:lng]
+    }
+  end
+
+  private
+
+  def conn
+    Faraday.get('https://maps.googleapis.com/maps/api/directions/json') do |req|
+      req.params[:origin] = @origin
+      req.params[:destination] = @destination
+      req.params[:key] = ENV['DIRECTIONS_KEY']
+    end
+  end
+end
